263f1db1d0a962a2ee7bc3e9f26e03bb563ed391
wranai <dakhota85@gmail.com>
2022-02-09 17:18:29 +0100

    zh => en [MT]

--- GFS/FIS/DecisionSystem.py
+++ GFS/FIS/DecisionSystem.py
@@ -1,9 +1,9 @@
 class DecisionSystem(object):
     def __init__(self, fuzzy_variable_list, rule_lib):
         """
-        FIS决策系统：由模糊变量集与模糊规则库组成
-        :param fuzzy_variable_list: 模糊变量集
-        :param rule_lib: 模糊规则库
+        FIS Decision System: Composed of Fuzzy Variable Set and Fuzzy Rule Base
+        :param fuzzy_variable_list: fuzzy variable set
+        :param rule_lib: Fuzzy rule library
         """
         self.fuzzy_variable_list = fuzzy_variable_list
         self.rule_lib = rule_lib
