9ebc5e45abcb42acef1ab08dd7efc7b2da957d06
wranai <dakhota85@gmail.com>
2022-02-09 17:19:53 +0100

    zh => en [MT]

--- GFS/FIS/DecisionSystemSimulation.py
+++ GFS/FIS/DecisionSystemSimulation.py
@@ -6,65 +6,65 @@ import math
 class DecisionSystemSimulation(object):
     def __init__(self, decision_system):
         """
-        使用相应的FIS决策系统进行仿真
-        :param decision_system: 决策系统
+        Simulation using the corresponding FIS decision system
+        :param decision_system: decision system
         """
         self.decision_system = decision_system
 
     def simulation_get_action(self, input_info):
         """
-        根据输入与规则库进行模糊推理，遍历整个规则库，找到最符合当前输入条件的规则，求出输出模糊变量具有最高置信度的模糊程度(Term)，将其索引作为决策行为索引。
-        :param input_info: 输入模糊变量的观测数值
-        :return: 决策行为索引
+        Carry out fuzzy inference according to the input and the rule base, traverse the entire rule base, find the rule that best matches the current input conditions, obtain the fuzzy degree (Term) of the output fuzzy variable with the highest confidence, and use its index as the decision behavior index.
+        :param input_info: the observed value of the input fuzzy variable
+        :return: decision behavior index
         """
-        rule_lib = self.decision_system.rule_lib  # 先获得FIS的规则库
-        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1]  # 输出模糊变量
-        action_value = [0 for _ in range(len(output_fuzzy_variable.terms))]   # 一个模糊程度（Term）对应一个action
+        rule_lib = self.decision_system.rule_lib # Get the FIS rule library first
+        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1] # Output fuzzy variables
+        action_value = [0 for _ in range(len(output_fuzzy_variable.terms))] # A fuzzy degree (Term) corresponds to an action
 
-        """ 遍历整个规则库，根据输入来判断当前状态（观测）最符合哪一条规则前件，继而计算输出模糊变量的哪一个 Term 置信度最高，置信度最高的 Term 则为最置信度最高的行为 """
+        """ Traverse the entire rule base, determine which rule antecedent the current state (observation) most conforms to according to the input, and then calculate which Term of the output fuzzy variable has the highest confidence, and the Term with the highest confidence is the one with the highest confidence behavior"""
         for rule in rule_lib:
             antecedent = rule.antecedent
             consequent = rule.consequent
-            strength = antecedent.compute_value(input_info)  # 计算规则强度
-            action_id = consequent.clause.id            # Term 的 id 即行为的 id
+            strength = antecedent.compute_value(input_info) # Calculate rule strength
+            action_id = consequent.clause.id # The id of the Term is the id of the action
             if action_id == -1:
                 continue
-            """ 多个规则可能对应同一个 action(Term)，取置信度（规则强度）最大的 action（Term）Value 作为该行为的 action value """
+            """ Multiple rules may correspond to the same action (Term), take the action (Term) Value with the largest confidence (rule strength) as the action value of the behavior """
             action_value[action_id] = max(action_value[action_id], strength)
 
-        return action_value.index(max(action_value))    # 返回最高置信度行为的行为索引
+        return action_value.index(max(action_value)) # Returns the action index of the highest confidence action
 
     def simulation_get_crisp_value(self, input_info):
         """
-        根据输入与规则库进行模糊推理，遍历整个规则库，找到最符合当前输入条件的规则，求出输出模糊变量具有最高置信度的模糊程度(Term)，并将该 Term 去模糊化成具体数值输出。
-        @param input_info: 输入模糊变量的观测数值
-        @return: consequent 经过去模糊化后的精确输出
+        Carry out fuzzy inference according to the input and the rule base, traverse the entire rule base, find the rule that best meets the current input conditions, obtain the fuzzy degree (Term) with the highest confidence in the output fuzzy variable, and defuzzify the Term into a specific numerical output.
+        @param input_info: Enter the observed value of the fuzzy variable
+        @return: accurate output of consequent after defuzzification
         """
-        rule_base = self.decision_system.rule_lib  # 先获得FIS的规则库
-        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1]  # 输出模糊变量
+        rule_base = self.decision_system.rule_lib # Get the rule base of FIS first
+        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1] # Output fuzzy variables
         action_num = len(output_fuzzy_variable.terms)
-        output_terms_list = [Term for _ in range(action_num)]                 # 一个模糊程度（Term）对应一个action
+        output_terms_list = [Term for _ in range(action_num)] # A fuzzy degree (Term) corresponds to an action
 
-        """ 将输出模糊变量的所有 Term 保存起来，一个 Term 对应一个 action """
+        """ Save all terms that output fuzzy variables, one term corresponds to one action """
         for k, v in output_fuzzy_variable.terms.items():
             output_terms_list[v.id] = v
 
         clip_value = [0 for _ in range(action_num)]
 
-        """ 遍历整个规则库，保存输出模糊变量所有模糊程度（Term）的置信度（规则强度） """
+        """ Traverse the entire rule base and save the confidence (rule strength) of all fuzzy degrees (Term) of the output fuzzy variables """
         for rule in rule_base:
             antecedent = rule.antecedent
             consequent = rule.consequent
-            strength = antecedent.compute_value(input_info)  # 计算规则强度
+            strength = antecedent.compute_value(input_info) # Calculate rule strength
             action_id = consequent.clause.id
             if action_id == -1:
                 continue
             clip_value[action_id] = max(clip_value[action_id], strength)
 
-        """ 使用每一个 Term 的 clip_value（规则强度）进行插值计算，求出所有 Terms 的联合图形 """
+        """ Use the clip_value (rule strength) of each Term to interpolate to find the joint graph of all Terms """
 
-        """ 用 clip 去截取三角隶属函数，交点的横坐标有可能是小数，但在 for 循环遍历横坐标时无法计算到小数，因此需手动求解交点横坐标 """
-        interp_universe = []  # 用隶属值（clip value）去截三角形，得到两个交点的横坐标
+        """ Use clip to intercept the triangular membership function, the abscissa of the intersection may be a decimal, but the decimal cannot be calculated when the for loop traverses the abscissa, so you need to manually solve the abscissa of the intersection """
+        interp_universe = [] # Use the clip value to truncate the triangle to get the abscissa of the two intersection points
         for term in output_fuzzy_variable.all_term():
             index = term.id
             clip = clip_value[index]
@@ -74,23 +74,23 @@ class DecisionSystemSimulation(object):
             if clip == 1:
                 continue
             if a != b:
-                interp_universe.append(clip * (b - a) + a)  # 利用相似三角形定理，求出左交点的横坐标：(x-a)/(b-a) = clip_v / 1
+                interp_universe.append(clip * (b - a) + a) # Use the similar triangle theorem to find the abscissa of the left intersection: (x-a)/(b-a) = clip_v / 1
             if b != c:
-                interp_universe.append(c - clip * (c - b))  # 利用相似三角形定理，求出右交点的横坐标：(c-x)/(c-b) = clip_v / 1
+                interp_universe.append(c - clip * (c - b)) # Use the similar triangle theorem to find the abscissa of the right intersection: (c-x)/(c-b) = clip_v / 1
 
-        """ 输出模糊变量原本各 Term 的 x 轴取值范围 """
+        """ Output the original x-axis value range of each Term of the fuzzy variable """
         normal_universe = [i for i in range(output_fuzzy_variable.universe_down, output_fuzzy_variable.universe_up + 1)]
-        """ 将交点横坐标并入到 Term 的 x 轴整数集中，为了方便后面 Mean of Max 方法计算最大 clip 值的横坐标集合 """
-        final_universe = np.union1d(interp_universe, normal_universe)   # np.union1d 求并集
+        """ Incorporate the abscissa of the intersection point into the x-axis integer set of Term, in order to facilitate the later Mean of Max method to calculate the abscissa set of the maximum clip value """
+        final_universe = np.union1d(interp_universe, normal_universe) # np.union1d union
 
-        """ 计算每一个 Term 在每一个横坐标点（整数点+两个交点）的隶属度  """
+        """ Calculate the membership of each Term at each abscissa point (integer point + two intersection points) """
         mf_value = [[] for _ in range(action_num)]
         for index_universe in range(len(final_universe)):
             for index_action in range(action_num):
                 mf_value[index_action].append(
                     output_terms_list[index_action].compute_membership_value(final_universe[index_universe]))
 
-        """ 所有 Term 的联合最大隶属度图形（最大y值） """
+        """ Joint maximum membership graph for all Term (maximum y value) """
         output_distribution = []
         for index_universe in range(len(final_universe)):
             Max = 0
@@ -100,7 +100,7 @@ class DecisionSystemSimulation(object):
                 Max = max(Max, mf_value[index_action][index_universe])
             output_distribution.append(Max)
 
-        """ 使用 mean of maximum 去模糊化分布，求所有对应y值等于最大y值的横坐标点的中心值 """
+        """ Use mean of maximum to defuzzify the distribution and find the center value of all the abscissa points whose y value is equal to the maximum y value """
         Max = max(output_distribution)
         count = 0
         sum_count = 0
